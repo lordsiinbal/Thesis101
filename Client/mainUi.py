9119601cd063768b84c7fee5233a6ff43c818c9d
topetope024 <kristoferbalang.alcantara@bicol-u.edu.ph>
2022-02-23 17:47:58 +0800

    fix

--- Client/mainUi.py
+++ Client/mainUi.py
@@ -277,17 +277,15 @@ class RoadSetUp1(QtWidgets.QMainWindow):#Road Setting Up Ui
         # roi.close()
         self.roadAddress="images/" + data[x]['roadID'] + ".jpg"
         # f = open("images/"+data[x]['roadID']+".txt", "r")
-        if not path.exists("image/"+ data[x]['roadID']+".txt"): #verifying if the file is exist in the directory
+        if not path.exists("images/"+ data[x]['roadID']+".jpg"): #verifying if the file is exist in the directory
             roadReplaced=str(data[x]['roadCaptured'].replace("[", "").replace("]","").replace(" ", "")) #eliminating the none number except comma
             roadConverted=numpy.fromstring(roadReplaced, dtype=int, sep=',') #converting from string to number and making it a numpy array
             # print(len(roadConverted))
             roadArr=roadConverted.reshape(720,1280, 3) #reshaping the array to 720xx1280
             # print(numpy.array(roadArr,dtype=numpy.uint32))
-                
-        
             # print(numpy.array(data[0]['roadCaptured'],dtype=numpy.int32))
-            
             cv2.imwrite(r'images/{}.jpg'.format(data[x]['roadID']),  numpy.array(roadArr,dtype=numpy.int32)) #writing the array to image with datatype int32
+        
         return self.roadAddress
 
     def loading(self):
@@ -298,7 +296,7 @@ class RoadSetUp1(QtWidgets.QMainWindow):#Road Setting Up Ui
         f.close()
         self.selectedROI= json.loads(self.selected['roadBoundaryCoordinates'])
         self.selectedROI= numpy.asarray(self.selectedROI,dtype=numpy.int32)
-        roadAddressReading = "image/"+ self.selected['roadID'] + ".txt"
+        roadAddressReading = "image/"+ self.selected['roadID'] + ".jpg"
         self.selectedRoadImage  = roadAddressReading
         self.selectedRoadID = self.selected['roadID']
         self.selectedRoadName = self.selected['roadName']
