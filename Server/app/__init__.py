f1f519902b6d4e1754d73526ab01cb30fba3c45f
topetope024 <kristoferbalang.alcantara@bicol-u.edu.ph>
2022-03-03 21:13:03 +0800

    test

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 # print(sys.path)
 
@@ -12,7 +13,7 @@ from .database import db
  
 def create_app(config_file = 'settings.py'):
     app =Flask(__name__)
-
+    CORS(app, supports_credentials=True)
     app.config["MONGODB_HOST"]= 'mongodb+srv://topetope:topetope1024_@cluster0.hicig.mongodb.net/db_detectCore?retryWrites=true&w=majority'
     # app.config["MONGODB_HOST"]='localhost:27017/db_detectCore'
     app.config.from_pyfile(config_file)
